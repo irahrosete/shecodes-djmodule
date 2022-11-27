8240a3568adc5fceab31bd1d31561b6245e425a2
irah rosete <irah.rosete@outlook.com>
2022-11-27 23:21:43 +1100

    create routes urls

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question, Choice
+
+# get questions and display them
+def index(request):
+    return render(request, 'polls/index.html')
