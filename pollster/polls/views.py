be89e2ce2c04547ec2f53d2d0464167b906c0b61
irah rosete <irah.rosete@outlook.com>
2022-12-02 18:40:35 +1100

    create navbar partials

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 
 from .models import Question, Choice
