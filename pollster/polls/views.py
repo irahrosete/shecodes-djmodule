744cd9159309eb287aea3c4e98f89f980965b3f6
irah rosete <irah.rosete@outlook.com>
2022-12-02 17:08:31 +1100

    create base and index templates for polls

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -4,4 +4,6 @@ from .models import Question, Choice
 
 # get questions and display them
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
