60a8a0b627e16c3593abd60e2e06ab15888ae138
irah rosete <irah.rosete@outlook.com>
2022-12-02 18:32:31 +1100

    complete vote view and url

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -20,3 +22,23 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', { 'question': question })
+
+# vote for a question choice
+def vote(request, question_id):
+    # print(request.POST['choice'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You did not select a choice.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # always return an HttpResponseRedirect after successfully deailing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
