1d211d6a58a69efccfa1d5e059cda7d8ba9e3772
irah rosete <irah.rosete@outlook.com>
2022-12-02 17:16:41 +1100

    create detail views and urls

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -7,3 +7,11 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+# show specific question and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/results.html', {  'question': question })
